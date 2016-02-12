eb5d0f57857cbd333b127683d9676fe9c41a4dc3
Damien Churchill <damoxc@gmail.com>
2016-02-12 13:26:57 +0000

    regsubst parameters in the incorrect order

--- manifests/params.pp
+++ manifests/params.pp
@@ -68,11 +68,11 @@ class ossec::params {
       }
     }
     'windows': {
-      $config_file = regsubst(sprintf('c:/Program Files (x86)/ossec-agent/ossec.conf'), '\\', '/')
+      $config_file = regsubst('c:/Program Files (x86)/ossec-agent/ossec.conf', '/', '\\', 'G')
       $config_owner = 'Administrator'
       $config_group = 'Administrators'
 
-      $keys_file = regsubst(sprintf('c:/Program Files (x86)/ossec-agent/client.keys'), '\\', '/')
+      $keys_file = regsubst('c:/Program Files (x86)/ossec-agent/client.keys', '/', '\\', 'G')
       $keys_mode = '0440'
       $keys_owner = 'Administrator'
       $keys_group = 'Administrators'
